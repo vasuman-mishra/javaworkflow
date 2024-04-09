d34f7614cbb132216b4062bef0c30aa635c178f2
Vasuman Mishra <83945524+vasuman-mishra@users.noreply.github.com>
2024-04-09 19:30:21 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,10 @@
+// Declares the HelloWorld class
+public class hello {
+    
+    // Main method - program execution starts here
+    public static void main(String[] args) {
+        
+        // Prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
